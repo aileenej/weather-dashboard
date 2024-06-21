1219f60f6fa559d3a7354f335109c4ec0968b925
AileenAJJ <aileenjohnsonlsp@gmail.com>
2024-06-21 15:10:25 -0500

    added basic api code to script file

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,11 @@
 // The persistent data will be the history of searched cities
 
 // I need functionality for the submission form
+
+const APIKey = "e7489becf00f32dd1d856bb063abf1cf";
+
+var city;
+
+const queryURL = 'http://api.openweathmap.org/data/2.5/weather?q=${city}&appid=${APIKey}';
+
+// Figure out how to assign user input to city variable
